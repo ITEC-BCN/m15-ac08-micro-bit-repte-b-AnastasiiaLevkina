e6799f08ba5ee25165fcc93978b40b4342af3a55
Anastasiia Lekvina <146827165+AnastasiiaLevkina@users.noreply.github.com>
2024-10-24 17:32:31 +0200

    Actualització pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
+let tempo = 0
+input.onButtonPressed(Button.A, function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    tempo = input.rotation(Rotation.Pitch)
+    music.play(music.stringPlayable("C5 B A G F E D C ", tempo), music.PlaybackMode.UntilDone)
 })
