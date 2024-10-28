bb7022d9b215a8243b59fe1ee0309530bcb2160a
Anastasiia Lekvina <146827165+AnastasiiaLevkina@users.noreply.github.com>
2024-10-28 16:13:21 +0100

    Terminado.

--- main.ts
+++ main.ts
@@ -3,6 +3,6 @@ input.onButtonPressed(Button.A, function () {
     music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.UntilDone)
 })
 input.onButtonPressed(Button.B, function () {
-    tempo = input.rotation(Rotation.Pitch)
+    tempo = Math.abs(input.rotation(Rotation.Pitch)) * 60
     music.play(music.stringPlayable("C5 B A G F E D C ", tempo), music.PlaybackMode.UntilDone)
 })
